a90a0c5729c04e27f77046b5d24d4c32ba92b80a
Hafizuddin <hafizuddin@outlook.sg>
2017-08-18 01:33:05 +0800

    Maven Project

--- TradeAnalyzerAppClient/appClientModule/Main.java
+++ TradeAnalyzerAppClient/appClientModule/Main.java
@@ -1,14 +1,55 @@
+import java.util.Properties;
+
+import javax.ejb.EJB;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NameClassPair;
+import javax.naming.NamingEnumeration;
+import javax.naming.NamingException;
+import com.intelligenceoverflow.ejb.ExtractorRemote;
 
 public class Main {
+	@EJB
+	private static ExtractorRemote er;
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("TEST");
+		
+		try {
+			// Create Properties for JNDI InitialContext.
+			Properties prop = new Properties();
+			prop.put(Context.INITIAL_CONTEXT_FACTORY,
+					org.jboss.naming.remote.client.InitialContextFactory.class.getName());
+			prop.put(Context.URL_PKG_PREFIXES, "org.jboss.ejb.client.naming");
+			prop.put(Context.PROVIDER_URL, "http-remoting://localhost:8080");
+			prop.put("jboss.naming.client.ejb.context", true);
+			System.out.println(prop);
+			
+
+			
+			// Create the JNDI InitialContext.
+			Context context = new InitialContext(prop);
+			NamingEnumeration<NameClassPair> list = context.list("");
+			while (list.hasMore()) {
+				System.out.println(list.next().getName());
+			}
+			extractLogDataBean(context);
+		} catch (NamingException ex) {
+			System.out.println("NamingException: " + ex.getMessage());
+		}
+	}
+	
+	public static void extractLogDataBean(Context context) {
+		try {
+			// Look-up the bean.
+			ExtractorRemote er = (ExtractorRemote) context
+					.lookup("TradeAnalyzer/TradeAnalyzerEJB/Extractor!com.intelligenceoverflow.ejb.ExtractorRemote");
+			// Invoke business methods on the bean.
+			er.extractLines(null);
+			
+		} catch (Exception ex) {
+			System.out.println("Exception: " + ex.getMessage());
+		}
 	}
 
-	/* (non-Java-doc)
-	 * @see java.lang.Object#Object()
-	 */
-	public Main() {
-		super();
-	}
 
 }
--- TradeAnalyzerEJB/ejbModule/com/intelligenceoverflow/ejb/Extractor.java
+++ TradeAnalyzerEJB/ejbModule/com/intelligenceoverflow/ejb/Extractor.java
@@ -1,12 +1,52 @@
 package com.intelligenceoverflow.ejb;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import javax.ejb.Local;
+import javax.ejb.Remote;
 import javax.ejb.Stateless;
 
-/**
- * Session Bean implementation class Extractor
- */
 @Stateless
+@Local(ExtractorLocal.class)
+@Remote(ExtractorRemote.class)
 public class Extractor implements ExtractorRemote, ExtractorLocal {
-	
+	public void extractLines(String path) {
+		// Open the file
+		FileInputStream fstream = null;
+		try {
+			fstream = new FileInputStream(
+					"C:\\Test.txt");
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
+
+		String strLine;
+
+		// Read File Line By Line
+		try {
+			while ((strLine = br.readLine()) != null) {
+				// Print the content on the console
+				System.out.println(strLine);
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		// Close the input stream
+		try {
+			br.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
 
 }
